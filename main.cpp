70d607efe4cca3be59e7cfeaee44281dcc161e5f
MohmeedSalah <Mohmedsalah11112002@gmail.com>
2026-07-25 00:18:12 +0300

    merge with main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     cout << "this is a master" << endl;
     cout << "this is a not staging" << endl;
+    cout << "this new line is added in master branch" << endl;
 
     return 0;
 }
