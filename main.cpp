9f56aa2f8122a3978c362f868f19b4e5b7d39560
MohmeedSalah <Mohmedsalah11112002@gmail.com>
2026-07-24 23:57:53 +0300

    merger with master

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
-    cout << "this is a stagingggg" << endl;
+    cout << "this is a master" << endl;
+    cout << "this is a not staging" << endl;
     return 0;
 }
