dba421b7c48fe5febbe0a4bcf054f2399911694c
MohmeedSalah <Mohmedsalah11112002@gmail.com>
2026-07-25 00:24:45 +0300

    agsdfgdas

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 int main()
 {
-    cout << "this is a master" << endl;
-    cout << "this is a not staging" << endl;
-    cout << "this new line is added in master branch" << endl;
+    cout << "this is a staging" << endl;
 
     return 0;
 }
